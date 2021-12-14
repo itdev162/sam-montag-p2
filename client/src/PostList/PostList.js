4f3ca5b9a5ece1d6a465ec39a3669475fb4f9bb0
samtheman43 <montags@gmatc.matc.edu>
2021-12-14 13:57:02 -0600

    Finished Activity 10

--- client/src/PostList/PostList.js
+++ client/src/PostList/PostList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PostListItem from './PostListItem';
 
-const PostList = props => {
+/*const PostList = props => {
     const { posts, clickPost, deletePost } = props;
     return posts.map(post => <PostListItem key={post.id} post={post} clickPost={clickPost} />);
-}
-/*const PostList = props => {
+}*/
+
+const PostList = props => {
     const { posts, clickPost, deletePost } = props;
     return posts.map(post => (
         <PostListItem 
@@ -13,8 +14,9 @@ const PostList = props => {
             post={post} 
             clickPost={clickPost}
             deletePost={deletePost}
+            editPost={editPost}
         />
     ));
-};*/
+};
 
 export default PostList;
