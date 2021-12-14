48d1511636326aa2258198ad484d196bdd7f46c9
samtheman43 <montags@gmatc.matc.edu>
2021-12-14 12:37:04 -0600

    Completed Activity 9

--- client/src/PostList/PostList.js
+++ client/src/PostList/PostList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PostListItem from './PostListItem';
 
 const PostList = props => {
-    const { posts, clickPost } = props;
+    const { posts, clickPost, deletePost } = props;
     return posts.map(post => <PostListItem key={post.id} post={post} clickPost={clickPost} />);
 }
 /*const PostList = props => {
